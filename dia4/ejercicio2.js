49cd9246062188d713afd467bb2e6a768e3c1f84
sergiotvidal <sergiotvidal2@gmail.com>
2019-04-07 21:28:43 +0200

    dia4/ejercicio2.js finished

--- dia4/ejercicio2.js
+++ dia4/ejercicio2.js
@@ -5,11 +5,35 @@
 // * El cobro mínimo son 10 EUR. Si los litros de agua a facturar son menor a 10 EUR, entonces la factura final será de 10 EUR.
 // **Escribe un programa que calcule el gasto de agua en un mes dado la cantidad de litros gastada**
 
-let price = 0;
+let billAmount = 0;
 
-function totalLitres(litres) {
-    if (litres <= 50) {
-        
+function waterBillCalculator(litres) {
+    if (litres > 50) {
+        if ((litres - 50) * 0.1 < 10) {
+            return console.log(`You consumed ${litres} l., but you didn't reach the minimum. Your water bill is 10 €.`);
+        } else {
+            if (litres <= 200) {
+                billAmount = (litres - 50) * 0.1
+                return console.log(`You consumed ${litres} l., your water bill is ${billAmount}€.`);
+            } else {
+                billAmount = (150 * 0.1) + ((litres - 200) * 0.3);
+                return console.log(`You consumed ${litres} l., your water bill is ${billAmount}€.`)
+            }
+        }
+
+    } else {
+        return console.log('Your consumption did not reach the minimum. Your water bill is 10 €.')
+    }
 };
 
-totalLitres(350);
+waterBillCalculator(15);
+waterBillCalculator(65);
+waterBillCalculator(159);
+waterBillCalculator(365);
+waterBillCalculator(101);
+waterBillCalculator(150);
+waterBillCalculator(149);
+waterBillCalculator(151);
+waterBillCalculator(200);
+waterBillCalculator(201);
+waterBillCalculator(199);
